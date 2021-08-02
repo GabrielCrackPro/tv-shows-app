ae72a47b61cb510b31b11641f3129cff233f4fa3
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-08-02 14:05:11 +0200

    Search added 💻

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
-const mostPopularContainer = document.querySelector(".most-popular-container");
+const mainContainer = document.querySelector(".most-popular-container");
+const searchForm = document.querySelector("#search-form");
 const API_URLS = {
   base: "https://www.episodate.com/api",
   popular: " https://www.episodate.com/api/most-popular?page=1",
-  search: "https://www.episodate.com/api/search?q=SHOW&page=1",
+  search: "https://www.episodate.com/api/search?q=",
   details: "https://www.episodate.com/api/show-details?q=",
 };
 
@@ -12,23 +13,22 @@ const getData = async (url) => {
   return data;
 };
 
-window,
-  (onload = async () => {
-    let popular = await getData(API_URLS.popular);
-    popular.tv_shows.forEach((show) => {
-      if (show.end_date == null) {
-        show.end_date = "Unknown";
-      }
-      console.log(show);
-      mostPopularContainer.innerHTML += `
+window.onload = async () => {
+  let popular = await getData(API_URLS.popular);
+  popular.tv_shows.forEach((show) => {
+    if (show.end_date == null) {
+      show.end_date = "Unknown";
+    }
+    console.log(show);
+    mainContainer.innerHTML += `
       <div class="d-flex p-2">
       <img src="${show.image_thumbnail_path}" alt="${
-        show.name
-      }" height="100" width="100">
+      show.name
+    }" height="100" width="100">
       <div class="d-flex flex-column p-2">
       <h3>${show.name} <a href="${
-        API_URLS.details + show.id
-      }" target="_blank" class="btn btn-primary btn-sm">Details</a></h3>
+      API_URLS.details + show.id
+    }" target="_blank" class="btn btn-primary btn-sm">Details</a></h3>
       <p>Network: ${show.network}</p>
       <p>Country: ${show.country}</p>
       <p>Start Date: ${show.start_date} - End Date: ${show.end_date}</p>
@@ -36,5 +36,14 @@ window,
       </div>
       </div>
       `;
-    });
   });
+};
+searchForm.addEventListener("submit", async (e) => {
+  const searchData = new FormData(searchForm);
+  const query = searchData.get("query");
+  const url = API_URLS.search + query + "&page=1";
+  mainContainer.innerHTML = "";
+  await getData(url);
+  searchForm.reset();
+  e.preventDefault();
+});
